1d93c6c860a035540761d3dcd863abd8733ac487
AlexCarracedo <115996770+AlexCarracedo@users.noreply.github.com>
2023-01-11 20:54:43 +0100

    Update ejercicio8.js

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,5 +1,4 @@
 function borrar(lista) {
-    var a=0;
     var elemento = prompt("Introduzca el elemento que quieres borrar de la lista:")
     for (var i=0; i<lista.length; i++) {
         if (elemento == lista[i]) {
