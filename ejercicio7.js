1f8c84529eb9a8201894611ac2f906d8c155cf27
AlexCarracedo <115996770+AlexCarracedo@users.noreply.github.com>
2023-01-12 14:23:09 +0100

    Add files via upload

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,13 +1,12 @@
 function comprobar(lista) {
-    var a=0;
     var elemento = prompt("Introduzca el elemento que quieres comprobar que esté en la lista:")
     for (var i=0; i<lista.length; i++) {
         if (elemento == lista[i]) {
-            console.log(elemento+" se encuentra en la lista.");
+            return true;
             a=1;
         }
     }
     if (a==0) {
-        console.log(elemento+" no se encuentra en la lista.")
+        return false;
     }
 }
